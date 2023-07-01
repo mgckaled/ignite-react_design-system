d330101e6a4c63ed96cb7267e3d6b380256854ca
mgckaled <marcelgckaled@gmail.com>
2023-07-01 19:55:40 -0300

    feat: integrate storybook with react package - show button storie

--- .github/docs/b_design_system_em_react.md
+++ .github/docs/b_design_system_em_react.md
@@ -3,3 +3,5 @@
 - `themeMap: {...defaultThemeMap, height: 'space', width: 'space',}` - associar tokens para determinadas propriedades. É importatne inserir o `defaultThemeMap` para não substituir as associações padrão.
 
 - `npx sb init --builder @storybook/builder-vite --type react --use-npm` - comando para implementar o _StoryBook_. `sb` = storybook, `init` = dar início, `--builder` = builder, `@storybook/builder-vite` = usar o vite por debaixo do panos (mais rápido), `--type react` = conversão do código
+  
+- `export type ButtonProps = ComponentProps<typeof Button>` - extrai as propriedades que um componente nativamente pode receber
--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,11 +1,7 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
+  stories: ["../src/stories/**/*.mdx", "../src/stories/**/*.tsx"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
   framework: {
     name: "@storybook/react-vite",
     options: {},
@@ -13,5 +9,6 @@ const config = {
   docs: {
     autodocs: "tag",
   },
-};
-export default config;
+}
+export default config
+
