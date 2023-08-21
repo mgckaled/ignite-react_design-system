d357c7750e2a695e379c0b9053922c2894b3cd7a
mgckaled <marcelgckaled@gmail.com>
2023-08-21 14:57:34 -0300

    ci: add docs workflow

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,13 +1,23 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
   stories: ["../src/pages/**/*.mdx", "../src/stories/**/*.tsx"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
+  ],
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
   docs: {
     autodocs: "tag",
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === "PRODUCTION") {
+      config.base = "/ignite-react_design-system/"
+    }
   },
 }
 export default config
